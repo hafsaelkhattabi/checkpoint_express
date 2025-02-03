6418f8802462546ea1c936f2d46e0cb07339e454
hafsaelkhattabi <hafssakhattabi35@gmail.com>
2025-02-03 15:03:18 +0100

    Update app.js

--- web/app.js
+++ web/app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.static(path.join(__dirname, 'public'))); 
-//app.use(hours);
+
 
 // defines a route handler for the root URL
 app.get('/', (req, res)=> {
