6f7d555bef85bfcd3066a394ce6d4c40567d1281
hafsaelkhattabi <hafssakhattabi35@gmail.com>
2025-02-04 14:30:44 +0100

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,40 +1,38 @@
 const express = require('express')
 const path = require('path')
 
-// creating an Express application
 const app = express()
 const port = 3000
 
-// tell Express to use the EJS
-app.set('view engine', 'ejs')
-// setting the directory where Express should look for my view templates
+// app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
-// the middleware for the time to show the web content 
 
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 
-// defines a route handler for the root URL
+
+// 
 app.get('/', (req, res)=> {
-    res.render('home')
+    res.sendFile(path.join(__dirname, 'views', 'home.html'))
 })
 
 app.get('/service', (req, res) => {
-    res.render('service')
+    res.sendFile(path.join(__dirname, 'views', 'service.html'))
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact')
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'))
 })
 
+// middleware 
 app.use((err, req, res, next) => {
     console.error(err.stack)
     res.send('something wrong')
 })
 
-// start the Express server in the ${port}
+// the port for work the project
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`)
 })
